dca1173c5730135878869fa3dfb30ea40af5c262
Aingei <aing36334@gmail.com>
2024-01-05 16:29:38 +0700

    eieija

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -1,21 +1,71 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
+void inputMatrix(double[][N]);
 
-void findLocalMax(const double [][N], bool [][N]);
+void findLocalMax(const double[][N], bool[][N]);
 
-void showMatrix(const bool [][N]);
+void showMatrix(const bool[][N]);
 
-int main(){
-	double A[N][N]; 
-	bool B[N][N]; 
+int main()
+{
+	double A[N][N];
+	bool B[N][N];
 	cout << "Input the matrix.....\n";
 	inputMatrix(A);
-	findLocalMax(A,B);
+	findLocalMax(A, B);
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
+}
+
+void inputMatrix(double A[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << "Row " << i + 1 << ": "; // wenwuk cin enter wen
+		for (int j = 0; j < N; j++)
+		{
+			cin >> A[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double a[][N], bool b[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		b[0][i] = 0;
+		b[i][0] = 0;
+		b[i][N - 1] = 0;
+		b[N - 1][i] = 0;
+	}
+	for (int i = 1; i < N - 1; i++)
+	{
+		for (int j = 1; j < N - 1; j++)
+		{
+			if (a[i][j] >= a[i - 1][j] && a[i][j] >= a[i + 1][j] && a[i][j] >= a[i][j - 1] && a[i][j] >= a[i][j + 1])
+			{
+				b[i][j] = 1;
+			}
+			else
+			{
+				b[i][j] = 0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool A[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << A[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
